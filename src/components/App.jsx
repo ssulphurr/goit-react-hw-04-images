98866539fc456d98fcac6efcf67ab26a16bbce88
ssulphurr <ssulphurr@gmail.com>
2023-05-28 23:41:36 +0300

    refactored App using hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import css from './App.module.css';
 import SearchForm from './Searchbar/Searchbar';
 import api from 'services/api';
@@ -6,81 +5,74 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Button from 'components/Button/Buttom';
 import Loader from './Loader/Loader';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 
-export class App extends Component {
-  state = {
-    searchInput: '',
-    images: [],
-    page: 1,
-    isLoading: false,
-    totalImages: null,
-    error: null,
-  };
+export function App() {
+  const [searchInput, setSearchInput] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalImages, setTotalImages] = useState(null);
+  const [error, setError] = useState(null);
 
-  handleFormSubmit = searchInput => {
-    if (searchInput !== this.state.searchInput) {
-      this.setState({ searchInput, images: [], page: 1 });
+  const handleFormSubmit = propSearchInput => {
+    if (propSearchInput === searchInput) {
+      return;
     }
+    setSearchInput(propSearchInput);
+    setImages([]);
+    setPage(1);
   };
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.searchInput !== this.state.searchInput ||
-      prevState.page !== this.state.page
-    ) {
-      this.setState({ isLoading: true });
+  useEffect(() => {
+    if (searchInput) {
+      setIsLoading(true);
 
-      const response = await api.fetchImages(
-        this.state.searchInput,
-        this.state.page
-      );
+      const fetchData = async () => {
+        const response = await api.fetchImages(searchInput, page);
 
-      try {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...response.data.hits],
-          totalImages:
-            Number(response.data.total) / Number(response.data.hits.length),
-        }));
-      } catch (error) {
-        this.setState({ error });
-      } finally {
-        this.setState({ isLoading: false });
-      }
+        try {
+          setImages([...images, ...response.data.hits]);
+          setTotalImages(
+            Number(response.data.total) / Number(response.data.hits.length)
+          );
+        } catch (error) {
+          setError(error);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+
+      fetchData();
     }
-  }
+  }, [searchInput, page]);
 
-  increasePage = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const increasePage = () => {
+    setPage(page + 1);
   };
 
-  render() {
-    const { searchInput, images, isLoading, totalImages, error } = this.state;
+  return (
+    <div className={css.App}>
+      <SearchForm onSubmit={handleFormSubmit} />
+      <ImageGallery images={images} />
 
-    return (
-      <div className={css.App}>
-        <SearchForm onSubmit={this.handleFormSubmit} />
-        <ImageGallery images={this.state.images} />
+      {isLoading ? <Loader /> : null}
 
-        {isLoading ? <Loader /> : null}
+      {images.length > 0 && <ImageGallery /> && totalImages > 1 && (
+        <Button onClick={increasePage} />
+      )}
 
-        {images.length > 0 && <ImageGallery /> && totalImages > 1 && (
-          <Button onClick={this.increasePage} />
-        )}
+      {searchInput && !images.length > 0 && !isLoading && (
+        <h2 style={{ marginLeft: 40 }}> 0 images found</h2>
+      )}
 
-        {searchInput && !images.length > 0 && !isLoading && (
-          <h2 style={{ marginLeft: 40 }}> 0 images found</h2>
-        )}
-
-        {error && (
-          <h2 style={{ marginLeft: 40 }}>
-            Something went wrong: {error.message}
-          </h2>
-        )}
-      </div>
-    );
-  }
+      {error && (
+        <h2 style={{ marginLeft: 40 }}>
+          Something went wrong: {error.message}
+        </h2>
+      )}
+    </div>
+  );
 }
 
 App.propTypes = {
